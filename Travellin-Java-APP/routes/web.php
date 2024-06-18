0c4f86a5d8397a4283095800bde2982c6111109e
adindaginta <153262652+adindaginta@users.noreply.github.com>
2024-06-18 23:29:43 +0700

    Update web.php

--- Travellin-Java-APP/routes/web.php
+++ Travellin-Java-APP/routes/web.php
@@ -16,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LandingController::class, 'index'])->name('landing');
 
+// profile
+Route::middleware('auth')->group(function() {
+    Route::get('/Testimoni/create', [LandingController::class, 'create'])->name('landing.create');
+    Route::post('/Testimoni', [LandingController::class, 'store'])->name('landing.store');
+});
+
+
 Route::get('/', function () {
     return view('welcome');
 });
