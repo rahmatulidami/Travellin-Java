e4e1dc5f154ccf63a74c48af4acaa6fdecc9515e
Rahmatul idami <rahmatul_i@mhs.unsyiah.ac.id>
2024-06-18 12:56:52 +0700

    Menghubungkan ke Database dan install npm

--- Travellin-Java-APP/vite.config.js
+++ Travellin-Java-APP/vite.config.js
@@ -4,7 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js',
+                'resources/js/filament-chart-js-plugins.js', // Include the new file in the `input` array so it is built
+            ],
+            
             refresh: true,
         }),
     ],
