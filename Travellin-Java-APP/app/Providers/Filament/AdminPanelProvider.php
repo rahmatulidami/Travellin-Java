81e663b2d7f4940c358e9a95e771ad414713570d
adindaginta <153262652+adindaginta@users.noreply.github.com>
2024-06-19 00:44:19 +0700

    Create AdminPanelProvider.php

---
+++ Travellin-Java-APP/app/Providers/Filament/AdminPanelProvider.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Providers\Filament;
+
+use App\Filament\Auth\Register;
+use App\Filament\Pages\Auth\EditProfile;
+use Filament\Forms\Components\FileUpload;
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Pages;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Widgets;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Jeffgreco13\FilamentBreezy\BreezyCore;
+use Joaopaulolndev\FilamentEditProfile\FilamentEditProfilePlugin;
+
+class AdminPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->default()
+            ->id('admin')
+            ->path('admin')
+            ->login()
+            ->registration(Register::class)
+            // ->profile(EditProfile::class)
+            ->colors([
+                'primary' => Color::Amber,
+            ])
+            ->brandLogo(asset('images/LOGO TRAVELLIN JAVA.png'))
+            ->brandLogoHeight('4rem')
+            ->favicon(asset('images/LOGO TRAVELLIN JAVA.png'))
+            ->brandName('Travellin Java')
+            ->font('poppins')
+            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
+            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            ->pages([
+                Pages\Dashboard::class,
+            ])
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            ->widgets([
+                Widgets\AccountWidget::class,
+
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                Authenticate::class,
+            ])
+            ->plugins([
+                BreezyCore::make()
+                    ->myProfile(
+                        shouldRegisterUserMenu: true, // Sets the 'account' link in the panel User Menu (default = true)
+                        shouldRegisterNavigation: true, // Adds a main navigation item for the My Profile page (default = false)
+                        navigationGroup: 'My Profile', // Sets the navigation group for the My Profile page (default = null)
+                        hasAvatars: true, // Enables the avatar upload form component (default = false)
+                        slug: 'my-profile' // Sets the slug for the profile page (default = 'my-profile')
+
+                    )
+                    ->avatarUploadComponent(fn () => FileUpload::make('avatar_url')->directory('avatars')
+                    ->image()
+                    ->previewable(false)), // Disable preview
+            ]);
+    }
+}
