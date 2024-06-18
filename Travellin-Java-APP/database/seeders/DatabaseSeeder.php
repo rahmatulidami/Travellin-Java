69a89059b3a700eb27967c47ede31115665f7e33
Rahmatul idami <rahmatul_i@mhs.unsyiah.ac.id>
2024-06-19 00:20:48 +0700

    database seeder

--- Travellin-Java-APP/database/seeders/DatabaseSeeder.php
+++ Travellin-Java-APP/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +17,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            UserSeeder::class,
+            PermissionSeeder::class,
+            RoleSeeder::class,
+        ]);
     }
 }
