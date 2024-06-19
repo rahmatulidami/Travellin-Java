c157e49983551e6ca28265a9ff02d66ef7318819
Rahmatul idami <rahmatul_i@mhs.unsyiah.ac.id>
2024-06-19 08:35:14 +0700

    menambahkan meta name

--- Travellin-Java-APP/resources/views/frontend/createTestimoni.blade.php
+++ Travellin-Java-APP/resources/views/frontend/createTestimoni.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="dicoding:email" content="rahmatul_i@mhs.usk.ac.id">
     <title>TravellinJava</title>
     <!-- Favicon-->
     <link rel="shortcut icon" href="{{ asset('images/LOGO TRAVELLIN JAVA.png') }}" type="image/x-icon">
--- Travellin-Java-APP/resources/views/frontend/show.blade.php
+++ Travellin-Java-APP/resources/views/frontend/show.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="dicoding:email" content="rahmatul_i@mhs.usk.ac.id">
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>TravellinJava</title>
